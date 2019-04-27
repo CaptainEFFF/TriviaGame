dfc66248bc1a448323e04bce626824a9558381d3
Captain <davidbanville4@gmail.com>
2019-04-26 20:46:02 -0400

    Game Finished

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,10 +5,10 @@ var trivia = [
      answers:["F-18", "F-5e", "F-14", "Mig-28"],
      correctAnswer: "F-14",
      factoids:[
-     "The F-18 the modern upgraded version of the F-14 and is still in use today.",
-     "The Northrop F-5e is the model of plane used to represent the fictional MiG-28s in the movie",
+     "Wrong! The F-18 the modern upgraded version of the F-14 and is still in use today.",
+     "Wrong! The Northrop F-5e is the model of plane used to represent the fictional MiG-28s in the movie",
      "Correct! The F-14 was the main jet seen in the movie, although piloted by professionals real actors were often in the rear seat.",
-     "The MiG-28 is a fictional aircraft and were actually Nortrop F-5e",
+     "Wrong! The MiG-28 is a fictional aircraft and were actually Nortrop F-5e",
      ],
      Image: ['assets/image/f18.jpg', 'assets/image/f5e.jpg','assets/image/f14.jpeg','assets/image/mig28.jpg']
     },
@@ -21,27 +21,27 @@ var trivia = [
      "Thats a negative Ghostrider, Johny Blaze is the identity to Marvels Ghost Rider",
      "Thats a negative Ghostrider, Space Ghost is a cartoon talk show host, you nerd",
      ],
-     Image: ['assets/image/KnightRider', 'assets/image/ghost.jpeg','assets/image/ghostrider','assets/image/spaceghost.jpeg']
+     Image: ['assets/image/KnightRider.jpg', 'assets/image/ghost.jpeg','assets/image/ghostrider.jpg','assets/image/spaceghost.jpeg']
 
     },
     {question:'What is the corret quote from the movie?',
      answers:["It needs more power!", "I wanna go fast!", "Gotta go fast!", "I feel the need, the need for speed!" ],
      correctAnswer: "I feel the need, the need for speed!",
      factoids:[
-     "Top Gear's Jeremy Clarkson is well known for his obsession with speed and POWER ",
-     "Help me Jesus! Help me Jewish God! Help me Allah! Help me Tom Cruise! Tom Cruise use your witchcraft on me to get the fire off me! Help me Oprah Winfrey!” — Ricky Bobby Talladega Nights",
-     "You're Too Slow!",
-     "Feel the need.",
+     "Wrong! Top Gear's Jeremy Clarkson is well known for his obsession with speed and POWER ",
+     "Wrong! Help me Jesus! Help me Jewish God! Help me Allah! Help me Tom Cruise! Tom Cruise use your witchcraft on me to get the fire off me! Help me Oprah Winfrey!” — Ricky Bobby Talladega Nights",
+     "Wrong! You're Too Slow!",
+     "Correct! Feel the Need!",
      ],
-     Image: ['assets/image/morepower.jpg', 'assets/image/wannagofast','assets/image/Gotta_go_fast.png','assets/image/theneedforspeed.gif']
+     Image: ['assets/image/morepower.jpg', 'assets/image/wannagofast.jpg','assets/image/Gotta_go_fast.png','assets/image/theneedforspeed.gif']
 
     },
     {question:'True or False| The custom F-5e jets called "MiG-28s" in the film were kept as Top Gun training jets?',
      answers:["True", "False"],
      correctAnswer: "True",
      factoids: [
-     "The Top Gun School continued to use them as the 'enemy' jets just like in the movie",
-     "The Top Gun School continued to use them as the 'enemy' jets just like in the movie",
+     "Correct! The Top Gun School continued to use them as the 'enemy' jets just like in the movie",
+     "Wrong! The Top Gun School continued to use them as the 'enemy' jets just like in the movie",
      ],
      Image: ['assets/image/blackbunny.jpg', 'assets/image/blackbunny.jpg']
 
@@ -50,10 +50,10 @@ var trivia = [
      answers:["ToTo","Kenny Loggins", "Tom Whitlock", "Sterling Archer"],
      correctAnswer: "Kenny Loggins",
      factoids:[
-     "ToTo was apporached to perfom the song but turned it down due to other reasons",
-     "He actually refused to do it on several occasions but was finally conviced by the director Tony Scott",
-     "Although he didn't perform the song he did write it specifically for the movie.",
-     "You're in the Danger Zone",
+     "Wrong! ToTo was apporached to perfom the song but turned it down due to other reasons",
+     "Correct! He actually refused to do it on several occasions but was finally conviced by the director Tony Scott",
+     "Wrong! Although he didn't perform the song he did write it specifically for the movie.",
+     "Wrong! You're in the Danger Zone",
      ],
      Image: ['assets/image/toto.jpg', 'assets/image/kennyloggins.jpeg','assets/image/tomwhitlock.jpeg','assets/image/archer.jpg']
 
@@ -62,20 +62,20 @@ var trivia = [
      answers:["True","False"],
      correctAnswer: "False",
      factoids:[
-    "He actually went to House of Motorcycles in El Cajon, California to learn. They taught him in the parking lot of their shop.",
-    "He actually went to House of Motorcycles in El Cajon, California to learn. They taught him in the parking lot of their shop.",
+    "Wrong! He actually went to House of Motorcycles in El Cajon, California to learn. They taught him in the parking lot of their shop.",
+    "Correct! He actually went to House of Motorcycles in El Cajon, California to learn. They taught him in the parking lot of their shop.",
      ],
-     Image: ['assets/image/motorcycle.jpg', 'assets/image/motorcycle.jpg']
+     Image: ['assets/image/motocycle.jpg', 'assets/image/motocycle.jpg']
 
     },
     {question:'Which callsign isnt from Top Gun?',
      answers:["Merlin", "Mailman", "Jester", "Iceman"],
      correctAnswer: "Mailman",
      factoids:[
-     "Merlin was Cougars R.I.O. and later Mavericks",
-     "Mailman is from the Top Gun parody Hot Shots",
-     "Jester was one of the Top Gun intructors.",
-     "Iceman is Mavericks rival throughout their training at Top Gun",
+     "Wrong! Merlin was Cougars R.I.O. and later Mavericks",
+     "Correct! Mailman is from the Top Gun parody Hot Shots",
+     "Wrong! Jester was one of the Top Gun intructors.",
+     "Wrong! Iceman is Mavericks rival throughout their training at Top Gun",
      ],
      Image: ['assets/image/Merlin.jpg', 'assets/image/mailman.jpg','assets/image/jester.jpg','assets/image/iceman.jpg']
 
@@ -84,10 +84,10 @@ var trivia = [
      answers:["USS Midway 41", "USS Enterprise 1701", "USS Enterprise 65", "USS Geral R Ford 78"],
      correctAnswer: "USS Enterprise 65",
      factoids:[
-     "The Midway is now a decomished carrier docked in San Diego and used as a Navy/Airforce museum.",
-     "The Enterprise 1701 is the name of the space craft used in Star Trek",
-     "Only the opening and closing sequences show the carrier from affar, the director Tony Scott wrote a check onboard the ship for the captain to get arial footage.",
-     "The Gerarld R Ford is a brand new carrier that is one of the longest and most expensive naval vessel in history",
+     "Wrong! The Midway is now a decomished carrier docked in San Diego and used as a Navy/Airforce museum.",
+     "Wrong! The Enterprise 1701 is the name of the space craft used in Star Trek",
+     "Correct! Only the opening and closing sequences show the carrier from affar, the director Tony Scott wrote a check onboard the ship for the captain to get arial footage.",
+     "Wrong! The Gerarld R Ford is a brand new carrier that is one of the longest and most expensive naval vessel in history",
      ],
      Image: ['assets/image/midway.jpg', 'assets/image/startrek.jpg','assets/image/enterprise65.jpg','assets/image/geraldford.jpg']
 
@@ -95,9 +95,9 @@ var trivia = [
     {question:'True or False| The Top Gun school imposes a fine on any trainee or staff member that quotes the movie?',
      answers:["True", "False"],
      correctAnswer: "True",
-     factiods:[
-     "The fine is only $5...per quote.",
-     "The fine is only $5...per quote.",
+     factoids:[
+     "Correct! Though its a small fine thats only $5...per quote.",
+     "Wrong! They fine $5 per quote.",
      ],
      Image: ['assets/image/ego.jpg', 'assets/image/ego.jpg']
 
@@ -117,17 +117,71 @@ var trivia = [
     
 ]
 
-var timer = 15;
+var timer = 10;
+var time;
 var correct = 0;
 var wrong = 0;
-var answered = 0;
+var missed = 0;
+var timerGoing = false;
 var x = 0;
+
+
+// Experimented with shuffleing the array for a random order, couldn't get it to work though.
+// function shuffle(){
+//     for(var i = trivia.Length -1 ; i > 0 ; i--){
+//         var j = Math.floor(Math.random()*(i+1));
+//         [trivia[i],trivia[j]]=[trivia[j],trivia[i]];
+//     }
+//     console.log(trivia)
+// }
+
+function timerStart(){
+
+    if(!timerGoing){
+    time = setInterval(timed,1000)
+    timerGoing = true;
+    }
+
+}
+
+function timed(){
+    $("#time").html("<h1> Time: " + timer + "</h1>")
+    timer --;
+
+    if(timer < 0){
+        timerStop();
+        $("#trivia").empty();
+        $('#trivia').html("<h1> Missed That One! </h1>");
+        setTimeout(displayQuestion,3000);
+        x++;
+        missed++;
+    }
+}
+
+function timerStop(){
+    timerGoing = false;
+    timer = 10;
+    clearInterval(time);
+}
+
+
 
 
 
 function displayQuestion(){
+   
+   if(x===10){
+    $('#trivia').empty();
+    $('#trivia').html("<h1> You got " + correct + " correct.</h1>");
+    $('#trivia').append("<h1> You got " + wrong + " wrong.</h1>");
+    $('#trivia').append("<h1> You missed " + missed + " questions.</h1>");
+   }
+   
+   
+   else{
     var q = trivia[x].question
     console.log(q);
+    timerStart();
     $('#trivia').empty();
     $('#trivia').html("<p>" + q + "</p>");
     var a = trivia[x].answers
@@ -136,8 +190,10 @@ function displayQuestion(){
         btn = $('<button>');
         btn.html(a[i]);
         btn.addClass('btn btn-primary btn-lg answer');
+        btn.attr("data-name", a[i]);
         $('#trivia').append(btn);
     }
+   }   
 }
 
 
@@ -145,11 +201,14 @@ function displayQuestion(){
 
 // $(".answer").on("click",function answerClicked(){
     function checker(){
-        console.log("Clicked")
-        console.log($(this).text());
+        var clickedButton = $(this).attr("data-name")
+        console.log("Clicked");
+        console.log("Name " + clickedButton);
         console.log(trivia[x].correctAnswer)
-        
-        if ($(this).text() === trivia[x].correctAnswer){
+        console.log("Question: " + x);
+       
+        if ($(this).attr("data-name") === trivia[x].correctAnswer){
+            timerStop();
             console.log("Correct!");
             console.log(trivia[x].answers.indexOf($(this).text()));
             var index = trivia[x].answers.indexOf($(this).text())
@@ -160,14 +219,15 @@ function displayQuestion(){
             $(photo).attr("src",trivia[x].Image[index]);
             $(photo).addClass("photo");
             $('#trivia').append(photo);
-            btn = $('<button>');
+            btn = $('</br> </br> <button>');
             btn.html("Next");
-            btn.addClass('btn btn-primary btn-lg');
+            btn.addClass('btn btn-primary btn-lg next');
             $('#trivia').append(btn);
             x++;
         }
         
         else{
+            timerStop();
             console.log("Wrong");
             console.log(trivia[x].answers.indexOf($(this).text()));
             var index = trivia[x].answers.indexOf($(this).text())
@@ -178,15 +238,20 @@ function displayQuestion(){
             $(photo).attr("src",trivia[x].Image[index]);
             $(photo).addClass("photo");
             $('#trivia').append(photo);
+            btn = $('</br> </br> <button>');
+            btn.html("Next");
+            btn.addClass('btn btn-primary btn-lg next');
+            $('#trivia').append(btn);
             x++;
             
         }
     };
     $(".start").on("click",function(){
-        displayQuestion()
+        displayQuestion();
     });
     
-    $(document).on("click",".answer",checker());
+    $(document).on("click",".answer",checker);
+    $(document).on("click",".next",displayQuestion,);
     
     
     
